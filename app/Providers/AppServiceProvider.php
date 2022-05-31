a0d647af68fd42aeca08f94b0107c178de39575c
faroliwi <abad.simon.adrian.smr@gmail.com>
2022-05-31 19:46:02 +0200

    Heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Forces https scheme in production (Heroku App)
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
