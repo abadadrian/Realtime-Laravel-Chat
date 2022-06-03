7aa4486ee6dcd5e9bbccd80eecfa2dea2d1fa07e
faroliwi <abad.simon.adrian.smr@gmail.com>
2022-06-03 08:16:52 +0200

    Fixes

--- resources/views/layouts/page_templates/guest.blade.php
+++ resources/views/layouts/page_templates/guest.blade.php
@@ -2,6 +2,5 @@
 <div class="wrapper wrapper-full-page">
   <div class="page-header login-page header-filter"  data-color="purple">
     @yield('content')
-    @include('layouts.footers.guest')
   </div>
 </div>
